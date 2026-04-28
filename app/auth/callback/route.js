1bd6291d8689b0a7b392b8130a6ea2cdce7cf379
jinushin <shinjinu38@gmail.com>
2026-04-28 19:40:40 +0900

    fix pkce auth flow

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -7,8 +7,8 @@ export async function GET(request) {
 
   if (code) {
     const supabase = createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+      process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ""
     );
     await supabase.auth.exchangeCodeForSession(code);
   }
